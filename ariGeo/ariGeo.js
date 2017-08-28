8b3bf7ddd2b8c18974f47c60f3f860b1fc29d728
Abimbola Olaitan <craftwordltd@gmail.com>
2017-08-28 15:30:00 +0100

    add more test case to the spec

--- ariGeo/ariGeo.js
+++ ariGeo/ariGeo.js
@@ -1,30 +1,35 @@
 const aritGeo =  (arr) => {
-    const diff = arr[1] - arr[0];
-    const ratio = arr[1] / arr[0];
+    if(Array.isArray(arr)){
+        const diff = arr[1] - arr[0];
+        const ratio = arr[1] / arr[0];
 
-    let arith = true;
-    let geo = true;
+        let arith = true;
+        let geo = true;
 
-    for(let i = 0; i < arr.length - 1; i++)
-    {
-        if( arr[i + 1] - arr[i] !== diff )
-            arith = false;
-        if(arr[i + 1] / ratio !== arr[i])
-            geo = false;
-        }
+        for(let i = 0; i < arr.length - 1; i++)
+        {
+            if( arr[i + 1] - arr[i] !== diff )
+                arith = false;
+            if(arr[i + 1] / ratio !== arr[i])
+                geo = false;
+            }
 
-    if(arr.length === 0) {
-            return 0
-    }else {
-        if(arith === true) {
-            return "Arithmetic";
+        if(arr.length === 0) {
+                return 0
+        }else {
+            if(arith === true) {
+                return "Arithmetic";
+            }
+            else if(geo === true){
+                return "Geometric";
+            }
+            else{
+                return -1;
+            }
         }
-        else if(geo === true){
-            return "Geometric";
-        }
-        else{
-            return -1;
-        }
+    }
+    else {
+        return "Invalid Argument";
     }
 };
 
--- spec/ariGeo-spec.js
+++ spec/ariGeo-spec.js
@@ -29,16 +29,48 @@ describe("Case for an empty array", () => {
 
 describe("Case for a geometric sequence", () => {
 
-it("should return `Geometric` for [2, 6, 18, 54, 162]", () => {
-    aritGeo([2, 6, 18, 54, 162]).should.be.equal('Geometric');
-      });
+    it("should return `Geometric` for [2, 6, 18, 54, 162]", () => {
+        aritGeo([2, 6, 18, 54, 162]).should.be.equal('Geometric');
+        });
 
-it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", () => {
-    aritGeo([0.5, 3.5, 24.5, 171.5]).should.be.equal('Geometric');
-      });
+    it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", () => {
+        aritGeo([0.5, 3.5, 24.5, 171.5]).should.be.equal('Geometric');
+        });
 
-it("should return `Geometric` for [−128, 64, −32, 16, −8]", () => {
-    aritGeo([-128, 64, -32, 16, -8]).should.be.equal('Geometric');
-      });
+    it("should return `Geometric` for [−128, 64, −32, 16, −8]", () => {
+        aritGeo([-128, 64, -32, 16, -8]).should.be.equal('Geometric');
+        });
 
+});
+
+describe("Case for neither arithmetic nor geometric sequence", () => {
+
+    it("should return -1 for [1, 2, 3, 5, 8]", () => {
+        aritGeo([1, 2, 3, 5, 8]).should.eql(-1);
     });
+
+    it("should return -1 for [1, 3, 6, 10, 15]", () => {
+        aritGeo([1, 3, 6, 10, 15]).should.eql(-1);
+        });
+
+    it("should return -1 for [1, 8, 27, 64, 125]", () => {
+        aritGeo([1, 8, 27, 64, 125]).should.eql(-1);
+        });
+
+});
+
+describe("Case for a  non array argument", () => {
+
+    it("should return Invalid Argument for integer 5", () => {
+        aritGeo(5).should.be.equal('Invalid Argument');
+        });
+
+    it("should return Invalid Argument for a float 10.4", () => {
+        aritGeo(10.4).should.be.equal('Invalid Argument');
+        });
+
+    it("should return Invalid Argument for a string", () => {
+        aritGeo("string").should.be.equal('Invalid Argument');
+        });
+
+});
